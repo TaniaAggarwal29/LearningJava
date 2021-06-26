eeef567ba902828081093e5628aa5e17e72294aa
TaniaAggarwal29 <goyaltania64@gmail.com>
2021-06-27 02:50:44 +0530

    more exercises on methods

---
+++ TeenNumberChecker/src/taniaaggarwal/TeenNumberChecker.java
@@ -0,0 +1,18 @@
+package taniaaggarwal;
+
+public class TeenNumberChecker {
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    public static boolean isTeen(int num) {
+        return (num>=13 && num<=19);
+    }
+
+    public static boolean hasTeen(int n1, int n2, int n3) {
+        return (isTeen(n1) || isTeen(n2) || isTeen(n3));
+    }
+}
